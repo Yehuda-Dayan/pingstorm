d1fce3914f70aea367dbfb93daa0f7d10bba15e0
zivmon <ziv.m.avoda@gmail.com>
2025-03-25 14:43:15 +0200

    add while loop for andless user use. finle code befor connecting to other scrip

--- cont_sys_ping.sh
+++ cont_sys_ping.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+user_choice1=1
+
+while [[ "$user_choice1" -eq 1 ]]; do
+
+#clear
 echo "pingstorm control panel"
 echo "-------------------------"
 echo "1. start pingstorm"
@@ -8,32 +13,41 @@ echo "3. check status"
 echo "4. show log"
 echo "-------------------------"
 
-
 read -p "entre your choice [1-4]:  " user_choice1     
 
 if [[ "$user_choice1" == "1" ]]; then
 echo "here"
-#                   entre here the link to start ping storm 
-
+#              entre here the link to start ping storm 
 
 elif [[ "$user_choice1" == "2" ]]; then
-
-echo "have a nice day, Pingstorm is off"
+echo "Pingstorm is off"
 user_choice1=0
 
 elif [[ "$user_choice1" == "3" ]]; then
-
 echo "Pingstorm is off"
+user_choice=1
 
 elif [[ "$user_choice1" == "4" ]]; then
-
-echo "no data to show"
+echo "show log"
+#          entre here the commend to show the log file
+user_choice=1
 
 else 
 echo "error"
+user_choice1=1
  
-
 fi
+done
+
+
+
+
+
+
+
+
+
+
 
 
 
