2773e8932cb41f88524d63ea39509ba3d6ac7125
Shai25 <shaiyneeman@gmail.com>
2025-03-27 16:06:32 +0200

    completed the project

--- cont_sys_ping.sh
+++ cont_sys_ping.sh
@@ -9,27 +9,22 @@ echo "pingstorm control panel"
 echo "-------------------------"
 echo "1. start pingstorm"
 echo "2. stop pingstorm" 
-echo "3. check status"
-echo "4. show log"
+echo "3. show log"
 echo "-------------------------"
 
 read -p "entre your choice [1-4]:  " user_choice1     
 
 if [[ "$user_choice1" == "1" ]]; then
 echo "here"
-./ping_websites_in_list.sh
+./visuals.sh
 
 elif [[ "$user_choice1" == "2" ]]; then
 echo "Pingstorm is off"
 user_choice1=0
 
 elif [[ "$user_choice1" == "3" ]]; then
-echo "Pingstorm is off"
-user_choice=1
-
-elif [[ "$user_choice1" == "4" ]]; then
 echo "show log"
-head -n 10 logfile.log
+head -n 10 judecsv.csv
 
 user_choice=1
 
