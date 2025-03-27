ee04692c63968944262409505cef8aa79f74ebc0
Shain25 <shaiyneeman@gmail.com>
2025-03-27 12:15:40 +0200

    Update ping_websites_in_list.sh

--- ping_websites_in_list.sh
+++ ping_websites_in_list.sh
@@ -3,14 +3,15 @@ websites=(www.google.com www.facebook.com www.tiktok.com www.youtube.com www.net
 
 echo pinging ${websites[@]}
 for site in ${websites[@]}; do
-results=$(ping -c 4 $site) #pings 4 times for each website and save as variable
-#echo $results
+
+results=$(ping -c 4 $site  | awk '{print $8}' | sed 's/[^0-9.]//g' ) #pings 4 times for each website and save as variable
+echo $results
 test=$? #check if works
-echo $test
-	if [ $test -eq 0 ]; then #explain if website is reachable
-	echo "host $site is reachable"
-	else
-	echo "host $site not found"
-	fi
+# echo $test
+#	if [ $test -eq 0 ]; then #explain if website is reachable
+#	echo "host $site is reachable"
+#	else
+#	echo "host $site not found"
+#	fi
 
 done
