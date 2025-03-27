e40aa1dc3205e30dcbdbd85fc063989d67dd9220
Shai25 <shaiyneeman@gmail.com>
2025-03-27 16:00:44 +0200

    csv

--- pingcsvlog.sh
+++ pingcsvlog.sh
@@ -2,16 +2,23 @@
 
 log_file=logfile.log
 csv=judecsv.csv
+visf=visuals.sh
+
 #log function
 write_log() {
 	echo "$(date + '%d-%m-%y %H:%M:%S') | $1" >> "$log_file"
 }
 
-echo "Service,Latency (ms)" > "$csv"
-for websites in "${!results[@]}"; do
-	echo "$websites,${results[$websites]}" >> "$csv"
-	write_log "Recorded $websites latency: ${results[$websites]} ms"
-done
+#echo $visf | sed -n '1p' '2p' '3p' '4p' > "$csv"
 
-write_log "Results exported to $csv"
+#sed -n '2,4p' "$visf" > "$csv"
+
+bash "$visf" | head -n 4 > "$csv"
+
+#for websites in "${!results[@]}"; do
+#	echo "$websites,${results[$websites]}" >> "$csv"
+#	write_log "Recorded $websites latency: ${results[$websites]} ms"
+#done
+
+#write_log "Results exported to $csv"
 
