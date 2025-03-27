80e088cf46430f158dfd2d395d9ef37ade584673
Yehuda-Dayan <dyehuda10@gmail.com>
2025-03-27 08:04:40 +0000

    csv, log functions created

--- pingcsvlog.sh
+++ pingcsvlog.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 log_file=logfile.log
-cvs=judecsv.csv
+csv=judecsv.csv
+#log function
+write_log() {
+	echo "$(date + '%d-%m-%y %H:%M:%S') | $1" >> "log_file"
+}
 
+echo "Service,Latency (ms)" > "$csv"
+for websites in "${!result[@]}"; do
+	echo "$websites,${result[$websites]}" >>"$csv"
+	write_log "Recorded $websites latency: ${result[$websites]} ms"
+done
+
+write_log "Results exported to $csv"
 
